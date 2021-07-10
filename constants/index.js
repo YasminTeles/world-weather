5c54c9a69547e52b7ed6800c63929cc9b689edc9
Yasmin Teles <teles.yasmin@gmail.com>
2021-07-10 20:45:16 -0300

    feat: add open weather map service

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
 export const GEO_URL = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"
+export const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/find"
+
+export const CLOSEST_CITIES = 15
+export const APPID = "e23b93a970fce060d41a8d2361833d09"
---
+++ services/openWeatherMap.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { get } from "lodash"
+
+import {
+  OPEN_WEATHER_MAP_URL,
+  CLOSEST_CITIES,
+  APPID,
+} from "../constants"
+
+const weatherFetch = (latitude, longitude) => axios
+  .get(OPEN_WEATHER_MAP_URL, {
+    params: {
+      lat: latitude,
+      lon: longitude,
+      cnt: CLOSEST_CITIES,
+      APPID,
+    },
+  })
+  .then((response) => {
+    const list = get(response, "data.list", [])
+    return list
+  })
+  .catch(() => [])
+
+export default weatherFetch
