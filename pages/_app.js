1624239da78e998b8640b56a0abad460247fb7c5
Yasmin Teles <teles.yasmin@gmail.com>
2021-07-10 16:18:16 -0300

    refactor: initial app with lint rules

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+
+import PropTypes from "prop-types"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.object.isRequired,
 }
 
 export default MyApp
