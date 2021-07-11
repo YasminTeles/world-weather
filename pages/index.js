1cd76cb04494c0aa3f9f90139333a346fda0322d
Yasmin Teles <teles.yasmin@gmail.com>
2021-07-10 23:46:17 -0300

    perf: memorize Home component

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
+import { memo } from "react"
+
 import Head from "next/head"
 
 import Cities from "../components/Cities"
 import SearchButton from "../components/SearchButton"
 import WorldMap from "../components/WorldMap"
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Word Weather</title>
-      </Head>
+const Home = () => (
+  <div>
+    <Head>
+      <title>Word Weather</title>
+    </Head>
 
-      <main>
-        <WorldMap />
-        <SearchButton />
-        <Cities />
+    <main>
+      <WorldMap />
+      <SearchButton />
+      <Cities />
+    </main>
+  </div>
+)
 
-      </main>
-    </div>
-  )
-}
+export default memo(Home)
