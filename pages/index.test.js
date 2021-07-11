69ffef94838ab158730755cd72a05d2b67b38067
Yasmin Teles <teles.yasmin@gmail.com>
2021-07-11 20:10:47 -0300

    tests: fix home title test

--- pages/index.test.js
+++ pages/index.test.js
@@ -5,8 +5,9 @@ import { shallow } from "enzyme"
 import Home from "./index"
 
 describe("Home", () => {
-  it("Deve conter o texto \"Welcome to Next.js!\" dentro de um H1 no componente Index", () => {
+  it("should have an title page", () => {
     const wrap = shallow(<Home />)
-    expect(wrap.find("h1").text()).toEqual("Welcome to Home Page!")
+
+    expect(wrap.find("title").text()).toEqual("Word Weather")
   })
 })
